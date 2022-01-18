a6caecdf3bb93c6a3d9fcd3c504feae1f7c545e0
billesa <billesand@gmail.com>
2022-01-18 11:12:16 +0000

    Temporary changes

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -4,13 +4,13 @@ from translator import englishToFrench, frenchToEnglish
 
 class TestFrenchToEnglish(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(englishToFrench(None), None) # test when None is given as input the output is None.
+        self.assertRaises(ValueError('text must be provided'), englishToFrench(None)) # test when None is given as input the output is None.
         self.assertEqual(englishToFrench('Hello'), 'Bonjour') # test when 'Hello' is given as input the output is 'Bonjour'.
         
 
 class TestEnglishToFrench(unittest.TestCase): 
     def test1(self): 
-        self.assertEqual(frenchToEnglish(None), None) # test when None is given as input the output is None.
+        self.assertRaises(ValueError('text must be provided'), frenchToEnglish(None)) # test when None is given as input the output is None.
         self.assertEqual(frenchToEnglish('Bonjour'), 'Hello') # test when 'Bonjour' is given as input the output is 'Hello'.
 
 unittest.main()
--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,3 +1,5 @@
+"""Module cretaes an instance of LanguageTranslatorV3 and defines methods for access
+"""
 import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
@@ -11,13 +13,15 @@ url = os.environ['url']
 
 authenticator = IAMAuthenticator(apikey)
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
 language_translator.set_service_url(url)
 
 def englishToFrench(englishText):
+    """Translates from english to french
+    """
     frenchText = language_translator.translate(
         text=englishText,
         model_id='en-fr'
@@ -25,6 +29,8 @@ def englishToFrench(englishText):
     return frenchText
 
 def frenchToEnglish(frenchText):
+    """Translates from french to english
+    """
     englishText = language_translator.translate(
         text=frenchText,
         model_id='fr-en'
