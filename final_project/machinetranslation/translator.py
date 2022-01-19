532c2803635415e224e03ade27e4fb69eaa31b32
billesa <billesand@gmail.com>
2022-01-19 06:34:52 +0000

    Temporary changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -1,7 +1,7 @@
 """Module cretaes an instance of LanguageTranslatorV3 and defines methods for access
 """
 import os
-import json
+#import json
 from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from dotenv import load_dotenv
